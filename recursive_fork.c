4963672a8436b8b6f7b078572f186cf59e7e4eb0
Ahmed Amine Boustani <ahmed.a.boustani@gmail.com>
2016-10-12 17:25:22 +0100

    Fork new processes recursively

--- recursive_fork.c
+++ recursive_fork.c
@@ -1,4 +1,4 @@
-#define _XOPEN_SOURCE 
+#define _XOPEN_SOURCE // Include definitions for Kill function
 #include <stdio.h>
 #include <pthread.h>
 #include <sys/wait.h>
@@ -23,28 +23,29 @@ void proc (int p) {
   if (child != 0) {
     printf("Spawning process %d pid %d \n", p, child);
     waitpid(child, &status, 0);
+    proc(p + 1);
   }
   else {
     pthread_t thr[NUMTHRD];
     for (int j = 1; j <= NUMTHRD; j++){
-      vals[j] = j;
-      pthread_create(&thr[j - 1], NULL, &thread_id, vals + j);
+      vals[j - 1] = j;
+      pthread_create(&thr[j - 1], NULL, &thread_id, vals + j - 1);
+      pthread_join(thr[j - 1], 0);
     }
-   for (int k = 1; k <= NUMTHRD; k++)
-      pthread_join(thr[k - 1], 0);
+//   for (int k = 1; k <= NUMTHRD; k++)
+//     pthread_join(thr[k - 1], 0);
   }
-  proc(p + 1);
-  kill (child, SIGTERM);
 }
 
 // Code for each thread n
+
 void *thread_id (void *n) {
   printf("I am thread pid %d.%d \n", (int) getpid(), *(int *)n);
-//  return NULL;
-  pthread_exit(0);
+  return NULL;
 }
 
 /**************** Main Function *********************/
 int main (int argc, char** argv) {
   proc(1);
+  return 0;
 }
